31e34153b1dcd3c6c0498f1675e3620ddbb12cc1
dubenju <dubenju@gmail.com>
2020-05-10 15:50:15 +0900

    Windows API

---
+++ HotKeyHook/HotKeyHook/HotKeyHook.cpp
@@ -0,0 +1,258 @@
+﻿// HotKeyHook.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "HotKeyHook.h"
+#include <stdio.h>
+
+#define MAX_LOADSTRING 100
+
+// グローバル変数:
+HINSTANCE hInst;								// 現在のインターフェイス
+TCHAR szTitle[MAX_LOADSTRING];					// タイトル バーのテキスト
+TCHAR szWindowClass[MAX_LOADSTRING];			// メイン ウィンドウ クラス名
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+HWND hgWnd;
+HHOOK myhook;
+
+/****************************************************************
+WH_KEYBOARD hook procedure
+鍵盤钩子处理过程
+****************************************************************/
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
+    const TCHAR *info = NULL;
+    TCHAR text[50], data[20];
+
+//    PAINTSTRUCT ps;
+    HDC hdc;
+
+    if (nCode >= 0)
+    {
+        if (wParam == WM_KEYDOWN)           info = TEXT("normal key down");
+        else if (wParam == WM_KEYUP)        info = TEXT("normal key up  ");
+        else if (wParam == WM_SYSKEYDOWN)   info = TEXT("system key down");
+        else if (wParam == WM_SYSKEYUP)     info = TEXT("system key up  ");
+
+        ZeroMemory(text, sizeof(text));
+        ZeroMemory(data, sizeof(data));
+        wsprintf(text, TEXT("%s - vkCode [%04d], scanCode [%04d]  "), info, p->vkCode, p->scanCode);
+        wsprintf(data, TEXT("char： %c  "), p->vkCode);
+
+        hdc = GetDC(hgWnd);
+        TextOut(hdc, 10, 10, text, wcslen(text));
+        TextOut(hdc, 130, 10, data, wcslen(data));
+        ReleaseDC(hgWnd, hdc);
+
+        int fAlt = 0x20000000 & lParam;
+        int fUp = 0x40000000 & lParam;
+        // if (nCode == HC_ACTION && !fUp && wParam == VK_F7) {
+        if (p->vkCode == 0x51) {
+            UnhookWindowsHookEx(myhook);
+            SendMessage(hgWnd, WM_CLOSE, wParam, lParam);
+            return TRUE;
+        }
+        if (wParam == WM_KEYDOWN && p->vkCode == 0x50) {
+            // run program;
+            STARTUPINFO si;
+            PROCESS_INFORMATION pi;
+            ZeroMemory(&si, sizeof(si));
+            si.cb = sizeof(si);
+            ZeroMemory(&pi, sizeof(pi));
+
+            si.dwFlags = STARTF_USESHOWWINDOW;
+            si.wShowWindow = SW_HIDE;
+            CreateProcess(_T(".\\run.bat"), NULL, NULL, NULL, false, 0, NULL, NULL, &si, &pi);
+        }
+
+    }
+
+    return CallNextHookEx(myhook, nCode, wParam, lParam);
+}
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: ここにコードを挿入してください。
+	MSG msg;
+	HACCEL hAccelTable;
+    TCHAR text[30];
+
+	// グローバル文字列を初期化しています。
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_HOTKEYHOOK, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// アプリケーションの初期化を実行します:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+        //printf(".\n");
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_HOTKEYHOOK));
+
+    // 设置键盘全局监听
+    myhook = SetWindowsHookEx(
+        WH_KEYBOARD_LL, // 监听类型【键盘】
+        KeyboardProc,   // 处理函数
+        hInstance,      // 当前实例句柄
+        0               // 监听窗口句柄(NULL为全局监听)
+        );
+
+    if (myhook == NULL)
+    {
+        wsprintf(text, TEXT("键盘监听失败！error : %d\n"), GetLastError());
+        MessageBox(NULL, text, TEXT("错误"), MB_OK);
+    }
+
+	// メイン メッセージ ループ:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  関数: MyRegisterClass()
+//
+//  目的: ウィンドウ クラスを登録します。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_HOTKEYHOOK));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_HOTKEYHOOK);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   関数: InitInstance(HINSTANCE, int)
+//
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します。
+//
+//   コメント:
+//
+//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
+//        メイン プログラム ウィンドウを作成および表示します。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // グローバル変数にインスタンス処理を格納します。
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   // ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+   hgWnd = hWnd;
+   return TRUE;
+}
+
+//
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的:    メイン ウィンドウのメッセージを処理します。
+//
+//  WM_COMMAND	- アプリケーション メニューの処理
+//  WM_PAINT	- メイン ウィンドウの描画
+//  WM_DESTROY	- 中止メッセージを表示して戻る
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 選択されたメニューの解析:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 描画コードをここに追加してください...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
